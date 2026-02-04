e3be0c1f225287820becf404cb8933bb7ce2b0c1
Pahuljot Matharoo <matharoopahuljot@gmail.com>
2026-02-03 20:21:31 -0500

    added login endpoint

--- ChatAppClient/HttpRequest.h
+++ ChatAppClient/HttpRequest.h
@@ -5,5 +5,14 @@ private:
 	httplib::Client m_client;
 public:
 	HTTPRequest(std::string url) : m_client(url) {}
-	std::size_t connectToServer(const std::string& username, const std::string& password) { return 1; };
+	std::size_t connectToServer(const std::string& username, const std::string& password) {
+		httplib::Params params {
+			{"username", username},
+			{"password", password}
+		};
+		if (auto response = m_client.Post("/login", params)) {
+			return 1;
+		}
+		return 0;
+	};
 };
